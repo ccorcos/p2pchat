45e70335b26989412ed2c086fc8cd01f49577788
Chet Corcos <ccorcos@gmail.com>
2020-01-09 22:58:45 -0800

    Basic message UI.

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,5 +1,55 @@
 import * as React from "react"
 
-export function App() {
-	return <div>Hello world</div>
+interface AppState {
+	inputValue: string
+	messages: Array<string>
+}
+
+export class App extends React.PureComponent<{}, AppState> {
+	state: AppState = { messages: [], inputValue: "" }
+
+	private handleChangeInput = e => {
+		this.setState({
+			...this.state,
+			inputValue: e.target.value,
+		})
+	}
+
+	private handleSend = () => {
+		// TODO: record to log via IPC
+		if (this.state.inputValue) {
+			this.setState({
+				messages: [...this.state.messages, this.state.inputValue],
+				inputValue: "",
+			})
+		}
+	}
+
+	private handleKeyDown = (e: React.KeyboardEvent) => {
+		if (e.key === "Enter") {
+			this.handleSend()
+		}
+	}
+
+	render() {
+		return (
+			<div style={{ width: "30em", margin: "2em auto" }}>
+				<div>
+					{this.state.messages.map((message, i) => {
+						return <div style={{ margin: "0.2em 0" }}>{message}</div>
+					})}
+					<div style={{ margin: "0.2em 0", display: "flex" }}>
+						<input
+							value={this.state.inputValue}
+							onChange={this.handleChangeInput}
+							onKeyDown={this.handleKeyDown}
+							style={{ marginRight: 4, flex: 1 }}
+							placeholder="Message..."
+						/>
+						<button onClick={this.handleSend}>send</button>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,12 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { App } from "./components/App"
+import { css } from "glamor"
+
+css.global("html, body, #root", {
+	margin: 0,
+	padding: 0,
+})
 
 async function main() {
 	const root = document.getElementById("root") as HTMLElement
